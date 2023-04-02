11fa4140384cf0326476a69613392397cd4dd167
Shahid Khan <shahidsaifi88@gmail.com>
2023-04-02 15:40:16 +0530

    video8

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,18 +1,41 @@
 import React, { useState } from 'react'
 
 export default function TextForm(props) {
-    const [text, setText] = useState("Enter text here");
+    const [text, setText] = useState("");
+    const [count, setCount] = useState(0);
 
-    const changeLowertoUpperCase = ()=>{
+    const changeUpCase = () => {
         setText(text.toUpperCase());
+    }
+    const changeLoCase = () => {
+        setText(text.toLowerCase());
+    }
+
+    const onChangeEvent = (event) => {
+        setText(event.target.value)
+        countWords(event.target.value);
+    }
+
+    const countWords = (str) => {
+        const arr = str.split(' ');
+        const length = arr.filter(word => word !== '').length;
+        setCount(length);
     }
 
     return (
         <>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={(event)=> setText(event.target.value)} rows="8"></textarea>
-                <button className="btn btn-primary mt-3" onClick={changeLowertoUpperCase}>Convert to UpperCase</button>
+                <textarea className="form-control mb-2" value={text} onChange={onChangeEvent} rows="8"></textarea>
+                <button className="btn btn-primary" onClick={changeUpCase}>Convert to UpperCase</button>
+                <button className="btn btn-primary mx-3" onClick={changeLoCase}>Convert to LowerCase</button>
+            </div>
+            <div className="my-3">
+                <h3>Your text summary</h3>
+                <p>{count} words and {text.length} characters</p>
+                <p>{(0.008 * count).toFixed(2)} Minutes read </p>
+                <h3>Preview</h3>
+                <p>{text}</p>
             </div>
         </>
     )
