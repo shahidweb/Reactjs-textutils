0e8858c57ecebe7ea5d657b146a67647d1f215d0
Shahid Khan <shahidsaifi88@gmail.com>
2023-04-07 15:50:56 +0530

    video10

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import TextForm from './components/TextForm';
 import Navbar from './components/Navbar';
+import About from './components/About';
 
 function App() {
   return (
     <>
-    <Navbar title="TextUtils2" aboutText="About TextUtils"/>
-    <div className="container">
-      <TextForm heading="Enter the text to analyze below"/>
-    </div>
+      <Navbar title="TextUtils2" aboutText="About TextUtils" />
+      <div className="container mt-3">
+        {/* <TextForm heading="Enter the text to analyze below" /> */}
+        <About />
+      </div>
     </>
   );
 }
